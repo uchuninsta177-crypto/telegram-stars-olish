3f0ff596eb7f067c77c8a4aa0642dd679ec2ef52
uchuninsta177-crypto <uchuninsta177@gmail.com>
2026-07-16 08:05:09 +0500

    Update server.py

--- server.py
+++ server.py
@@ -1,10 +1,21 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
-app = Flask(__name__)
+app = Flask(name)
 
 @app.route("/")
 def home():
     return "Server ishlayapti!"
 
-if __name__ == "__main__":
+@app.route("/order", methods=["POST"])
+def order():
+    data = request.json
+
+    print("Yangi buyurtma:", data)
+
+    return jsonify({
+        "success": True,
+        "message": "Buyurtma qabul qilindi"
+    })
+
+if name == "main":
     app.run(host="0.0.0.0", port=5000)
