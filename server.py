8cf977b55642ccf155c731877c9d6ed998a5ac7d
uchuninsta177-crypto <uchuninsta177@gmail.com>
2026-07-16 19:38:12 +0500

    Update server.py

--- server.py
+++ server.py
@@ -1,8 +1,13 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import requests
+import os
 
-app = Flask(__name__)
+app = Flask(name)
 CORS(app)
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = "-1004457471821"
 
 @app.route("/")
 def home():
@@ -12,12 +17,29 @@ def home():
 def order():
     data = request.json
 
-    print("Yangi buyurtma:", data)
+    username = data.get("username")
+    stars = data.get("stars")
+    total = data.get("total")
+
+    text = f"""🛒 Yangi buyurtma
+
+👤 Username: {username}
+⭐ Stars: {stars}
+💰 Summa: {total:,} so'm
+"""
+
+    requests.post(
+        f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
+        json={
+            "chat_id": CHAT_ID,
+            "text": text
+        }
+    )
 
     return jsonify({
         "success": True,
         "message": "Buyurtma qabul qilindi"
     })
 
-if __name__ == "__main__":
+if name == "main":
     app.run(host="0.0.0.0", port=5000)
