5ed917215549260aa1855591eae3b378d7273735
uchuninsta177-crypto <uchuninsta177@gmail.com>
2026-09-03 22:41:27 +0500

    Add files via upload

--- bot.py
+++ bot.py
@@ -13,7 +13,7 @@ from telegram.ext import (
     filters,
 )
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+TOKEN = "8734389060:AAEDSlwkPfr9_TwLHkc1fWzN2AVOV3EfS7Q"
 GROUP_ID = -1004457471821
 ADMIN_ID = 8061937333
 
